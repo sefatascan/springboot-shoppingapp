34c9696cb07dc488b762294d10510e8b824c7c9f
stascan <sefa.tascan@btc-ag.com.tr>
2021-01-01 15:06:04 +0300

    initial

---
+++ src/test/java/com/stascan/shoppingapp/ShoppingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stascan.shoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
